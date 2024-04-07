d1e579969574c4a07f6ff8b239aa1fd1b0e92f10
David Solet <dsolet@gmail.com>
2024-04-06 21:01:48 -0700

    Updated to add in google libraries and access the non-RAW files.

--- access_hosp.R
+++ access_hosp.R
@@ -13,6 +13,8 @@
 library(httr)
 library(jsonlite)
 library(dplyr)
+library(googledrive)
+library(googlesheets4)
 
 # Function to retrieve all data
 retrieve_all_data <- function(url) {
@@ -54,8 +56,39 @@ retrieve_all_data <- function(url) {
 
 # API endpoint (SODA 2.1)
 # note filter to Massachusetts in url
-url <- "https://healthdata.gov/resource/uqq2-txqb.json?state=MA"
+url <- "https://healthdata.gov/resource/anag-cw7u.json?state=MA"
 
 # Retrieve all data
 patientImpact_MA <- retrieve_all_data(url)
+colnames(patientImpact_MA)
+CamHlthAll <- filter(patientImpact_MA , hospital_name == 'CAMBRIDGE HEALTH ALLIANCE')
+MtAub <- filter(patientImpact_MA , hospital_name == 'MOUNT AUBURN HOSPITAL')
 
+
+############### UPLOAD DATA FILE TO GOOGLE DRIVE ###############
+# Interact with Google Drive
+# Authorize connection: start with clean authorization slate
+gs4_deauth()
+gs4_user()
+
+# set up authorization
+drive_auth(email = "dsolet@gmail.com")
+gs4_auth(token = drive_token())
+
+# upload hospital datasets to Drive and move it to desired location
+# Cambridge Health Alliance
+gs4_create("Patient impact Cambridge Health All", sheets = list(CamHlthAll %>% select(-geocoded_hospital_address)))
+# for path = as_id, insert url for the destination folder
+drive_mv(
+  file = as_id(drive_find(pattern = "Patient impact Cambridge Health All", n_max = 1)),
+  path = as_id("https://drive.google.com/drive/u/0/folders/1mN2Fl-WbB1nGZbvF-ccoVbBrR8WgPu1C"),
+  overwrite = TRUE
+)
+
+gs4_create("Patient impact Mount Auburn Hospital", sheets = list(MtAub %>% select(-geocoded_hospital_address)))
+# for path = as_id, insert url for the destination folder
+drive_mv(
+  file = as_id(drive_find(pattern = "Patient impact Mount Auburn Hospital", n_max = 1)),
+  path = as_id("https://drive.google.com/drive/u/0/folders/1mN2Fl-WbB1nGZbvF-ccoVbBrR8WgPu1C"),
+  overwrite = TRUE
+)
